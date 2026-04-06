0cb385d836db98c947d96399c012e406893df56e
Vlad Bogodyazh <liquid98@yandex.by>
2026-04-06 15:23:20 +0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import { Montserrat, Raleway } from 'next/font/google';
+import './styles/globals.css';
+
+const montserrat = Montserrat({
+  subsets: ['cyrillic', 'latin'],
+  variable: '--font-montserrat',
+  weight: ['400', '500', '600', '700'],
+});
+
+const raleway = Raleway({
+  subsets: ['cyrillic', 'latin'],
+  variable: '--font-raleway',
+  weight: ['700'],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="ru"
+      className={`${montserrat.variable} ${raleway.variable} h-full`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
